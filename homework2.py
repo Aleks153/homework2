fcd8c0c18026a8168b5537a90818362beaaef7b1
Aleks153 <lermontov.v.pyatigorske@yandex.ru>
2024-05-26 00:45:15 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,14 @@
+print('Variant №1')
+dom_zad = 12
+zatrach_vremya = 1.5
+nazv_kurs = 'Python'
+vremya_na_zad = zatrach_vremya / dom_zad
+print(nazv_kurs+",",'всего задач:',dom_zad,',','затрачено часов:',zatrach_vremya,',','среднее время выполнения',vremya_na_zad,'часа.')
+
+print('Variant №2')
+dom_zad = 12
+ZatrachVremya = 1.5
+nazv_kurs = 'Python'
+VremyaNaZad = ZatrachVremya / dom_zad
+a = ", "
+print(str(nazv_kurs)+a+'всего задач:'+str(dom_zad)+a+'затрачено часов: '+str(ZatrachVremya)+a+'среднее время выполнения '+str(VremyaNaZad)+' часа.')
